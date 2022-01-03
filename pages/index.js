fca099f2b5b21c9a8985be4775b988b65bfbeee5
Ari Wiradana <ari@limecommerce.com>
2022-01-03 19:46:11 +0800

    logo back

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,7 @@ import { logo } from '../public/assets/img'
 export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-bl from-indigo-500 via-purple-500 to-pink-500">
-      {/* <img src={logo.src} alt="logo" className="w-60 h-60 animate-pulse" /> */}
-      <p className="text-3xl">Hello</p>
+      <img src={logo.src} alt="logo" className="w-60 h-60 animate-pulse" />
     </div>
   );
 }
