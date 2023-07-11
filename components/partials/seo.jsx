b9bca75fef21f9193e179f3a912119da13abdd9f
Ari Wiradana <ariwiradana5@gmail.com>
2023-07-12 06:16:37 +0800

    disable auto zoom

--- components/partials/seo.jsx
+++ components/partials/seo.jsx
@@ -5,6 +5,7 @@ const Seo = ({ title }) => {
   return (
     <Head>
       <title>{title || ""}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
       <meta name="description" content="Notsokoplo's gigs and gallery" />
       <meta name="keywords" content="Notsokoplo" />
       <meta name="robots" content="index, follow" />
