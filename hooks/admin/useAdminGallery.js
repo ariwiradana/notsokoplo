70f2fc5a7b2397c112d7c767cfb1fd1a62194041
Ari Wiradana <ariwiradana5@gmail.com>
2023-07-16 15:06:17 +0800

    sdasd

--- hooks/admin/useAdminGallery.js
+++ hooks/admin/useAdminGallery.js
@@ -49,7 +49,7 @@ const useAdminGallery = () => {
       let imageLeft = [];
       let skipImage = 0;
 
-      if (value?.length >= 15) {
+      if (value?.length > 15) {
         toast.warning("Photos cannot be more than 15");
         document.getElementById("edit-image").value = "";
         setDetail({ ...detail, images: [] });
@@ -77,7 +77,7 @@ const useAdminGallery = () => {
     if (path == "images" && value) {
       let imageLeft = [];
       let skipImage = 0;
-      if (value?.length >= 15) {
+      if (value?.length > 15) {
         toast.warning("Photos cannot be more than 15");
         document.getElementById("add-images").value = "";
         setValues({ ...values, images: [] });
