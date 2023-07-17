e690ec50184dc021d286ee719c085ad77c0e4d29
Ari Wiradana <ariwiradana5@gmail.com>
2023-07-17 11:30:18 +0800

    updarte image grid size

--- components/partials/gallery.jsx
+++ components/partials/gallery.jsx
@@ -9,7 +9,7 @@ import moment from "moment";
 import Image from "next/image";
 
 const GalleryComponent = () => {
-  const { images, isLoading, data } = useGallery();
+  const { isLoading, data } = useGallery();
   const { position } = useNavbar();
 
   return (
@@ -17,38 +17,6 @@ const GalleryComponent = () => {
       <Seo title="Notsokoplo | Gallery" />
       <Loading isLoading={isLoading} />
       <Layout>
-        {/* <div
-          className={`grid grid-cols-2 md:grid-cols-3 gap-1 p-1 transition-all ease-in-out duration-500 min-h-screen ${
-            position > 10 ? "md:mt-16 mt-12" : "md:mt-24 mt-16"
-          }`}
-        >
-          {images?.map((image, index) => (
-            <div className="grid gap-1" key={`row-${index}`}>
-              {image?.map(({ image, alt, title, date, path }) => (
-                <div
-                  key={alt}
-                  className="w-full bg-cover md:max-h-full overflow-hidden relative group cursor-pointer"
-                >
-                  <Link href={`/gallery/${path}`}>
-                    <img
-                      className="h-full w-full md:max-h-full max-w-full group-hover:grayscale object-cover transform group-hover:scale-110 transition-all ease-in-out duration-500"
-                      src={image}
-                      alt={alt}
-                    />
-                    <div className="absolute max-h-[30vh] md:max-h-full bottom-0 inset-x-0 p-3 md:px-4 md:py-3 lg:py-6 lg:px-8 items-center flex-col bg-black bg-opacity-70 delay-100 transition-all ease-in-out duration-500">
-                      <h5 className="font-montserrat text-white font-medium text-sm leading-5 md:text-lg lg:text-xl md:tracking-wide capitalize">
-                        {title}
-                      </h5>
-                      <h5 className="font-raleway text-white font-light md:mt-0 lg:tracking-widest text-[10px] md:text-xs uppercase">
-                        {moment(date).format("dddd, D MMMM YYYY")}
-                      </h5>
-                    </div>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div> */}
         <div
           className={`grid grid-cols-2 md:grid-cols-3 gap-1 p-1 transition-all ease-in-out duration-500 ${
             position > 10 ? "md:mt-16 mt-12" : "md:mt-24 mt-16"
@@ -57,7 +25,7 @@ const GalleryComponent = () => {
           {data?.data?.map((el) => (
             <Link
               href={`/gallery/${el?.data?.path}`}
-              className="w-full bg-cover group overflow-hidden relative group border h-full min-h-[45vh] md:min-h-[40vh] lg:min-h-[40vh]"
+              className="w-full bg-cover group overflow-hidden relative group border h-full min-h-[35vh] md:min-h-[40vh] lg:min-h-[40vh]"
               key={el?.data?._id}
             >
               <Image
