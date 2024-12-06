3e7f55ff32ba192a351966b59e543eb7f47a3b4f
Ari Wiradana <ariwiradana5@gmail.com>
2024-12-07 06:12:55 +0800

    modified:   lib/google-sheet.ts

--- lib/google-sheet.ts
+++ lib/google-sheet.ts
@@ -3,7 +3,7 @@ import { JWT } from "google-auth-library";
 
 const serviceAccountAuth = new JWT({
   email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  key: process.env.GOOGLE_PRIVATE_KEY?.split(String.raw`\n`).join("\n") || "",
+  key: process.env.GOOGLE_PRIVATE_KEY || "",
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
