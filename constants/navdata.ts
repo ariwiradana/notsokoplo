0d52f9a1082ea0509ba58cc38faf704e04ed45c8
Ari Wiradana <ariwiradana5@gmail.com>
2024-12-11 01:02:56 +0800

    modified:   constants/navdata.ts

--- constants/navdata.ts
+++ constants/navdata.ts
@@ -1,8 +1,8 @@
 import { Nav } from "@/types/nav";
 
 export const NavData: Nav[] = [
-  { path: "music", title: "Music" },
-  { path: "biography", title: "Biography" },
   { path: "events", title: "Events" },
+  { path: "biography", title: "Biography" },
+  { path: "music", title: "Music" },
   { path: "contact", title: "Contact" },
 ];
