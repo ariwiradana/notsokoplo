f00bbbf582902d2f9d3fd596b4961809eab0a285
Ari Wiradana <ariwiradana5@gmail.com>
2024-12-11 12:31:50 +0800

    modified:   pages/contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,25 +1,7 @@
 import Seo from "@/components/layout/seo";
-import { useRouter } from "next/router";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 const ContactPage = () => {
-  const router = useRouter();
-  const hasDownloaded = useRef(false);
-
-  useEffect(() => {
-    if (!hasDownloaded.current) {
-      const link = document.createElement("a");
-      link.href = "/pdf/NOT SO KOPLO PROFILE.pdf";
-      link.download = "NOT SO KOPLO PROFILE.pdf";
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-
-      hasDownloaded.current = true;
-      router.push("/");
-    }
-  }, [router]);
-
   return (
     <>
       <Seo
