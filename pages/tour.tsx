26e0ff581c0a1be69e2f907dd015be1ed5c03286
Ari Wiradana <ariwiradana5@gmail.com>
2024-12-11 12:48:50 +0800

    deleted:    pages/home.tsx new file:   pages/tour.tsx

---
+++ pages/tour.tsx
@@ -0,0 +1,18 @@
+import Seo from "@/components/layout/seo";
+import React from "react";
+
+const Tour = () => {
+  return (
+    <>
+      <Seo
+        url="https://notsokoplo.com/tour"
+        title="Tour | No So Koplo Official Website"
+        description="Learn more about Not So Koplo's tour."
+        image="https://res.cloudinary.com/dta5qasmt/image/upload/v1733891027/image_home_xmgpr1.jpg"
+        keywords="notsokoplo, not so koplo, tour"
+      />
+    </>
+  );
+};
+
+export default Tour;
