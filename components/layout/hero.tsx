12cbf4139b3d41b36238767ed527538d78ff35aa
Ari Wiradana <ariwiradana5@gmail.com>
2024-12-11 14:22:44 +0800

    modified:   components/layout/hero.tsx

--- components/layout/hero.tsx
+++ components/layout/hero.tsx
@@ -10,23 +10,22 @@ const HeroComponent = () => {
       className="h-svh w-full relative z-10 flex items-center justify-center"
       id="home"
     >
-      <SquareLoader loading={isLoading} color="white" />
+      {isLoading && <SquareLoader color="white" />}
       <div className="absolute inset-0 w-full h-full overflow-hidden">
         <video
+          preload="auto"
           onLoadedData={handleIsLoading}
           className="min-w-full min-h-full absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 object-cover"
           autoPlay
           muted
           loop
           playsInline
+          poster="https://res.cloudinary.com/dta5qasmt/image/upload/v1733898081/hero6_wflplg.jpg"
         >
           <source src="https://res.cloudinary.com/dta5qasmt/video/upload/v1733882455/NSK_Bali_Countdown_2023_1_fhfgd4.mp4" />
         </video>
       </div>
-
-      {!isLoading && (
-        <div className="absolute inset-0 bg-gradient-to-b from-dark/60 via-[30%] via-transparent to-dark z-10"></div>
-      )}
+      <div className="absolute inset-0 bg-gradient-to-b from-dark/60 via-[30%] via-transparent to-dark z-10"></div>
     </div>
   );
 };
