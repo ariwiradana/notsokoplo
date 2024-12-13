0be093f0f083e8fd83e8b87d79f8c4ee17417fac
Ari Wiradana <ariwiradana5@gmail.com>
2024-12-13 14:37:33 +0800

    new file:   next-sitemap.config.js modified:   package.json new file:   public/robots.txt new file:   public/sitemap-0.xml new file:   public/sitemap.xml modified:   yarn.lock

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+
+module.exports = {
+  siteUrl: "https://www.notsokoplo.com", // Ganti dengan URL situs Anda
+  generateRobotsTxt: true, // Tetap menghasilkan robots.txt
+  changefreq: "daily", // Frekuensi update, misal 'daily'
+  priority: 0.7, // Prioritas default untuk halaman
+};
