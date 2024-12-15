cf9a947930b3c1519f3724c340a0e9f79c772ba5
Ari Wiradana <ariwiradana5@gmail.com>
2024-12-15 14:14:34 +0800

    modified:   components/layout/instagram.feed.tsx

--- components/layout/instagram.feed.tsx
+++ components/layout/instagram.feed.tsx
@@ -7,7 +7,7 @@ const InstagramFeedComponent = () => {
       id="gallery"
       className={`relative bg-black overflow-x-hidden ${montserrat.className} z-0`}
     >
-      <div className="pt-16 lg:pb-8 lg:pt-28">
+      <div className="pt-16 lg:pb-8 lg:pt-28 z-0">
         <div
           className={`flex flex-col md:flex-row md:items-center justify-between mb-4 md:mb-12 gap-4 md:gap-12 max-w-screen-xl mx-auto px-4 md:px-12 lg:px-4 ${montserrat.className}`}
         >
@@ -29,8 +29,8 @@ const InstagramFeedComponent = () => {
           className="elfsight-app-dc7b977e-154a-4103-846e-63aa9d1d3613 mb-16 relative"
           data-elfsight-app-lazy
         ></div>
-        <div className="w-full h-16 lg:h-28 absolute bottom-0 bg-black z-[999]"></div>
       </div>
+      <div className="w-full h-16 lg:h-28 absolute bottom-0 bg-black z-[999]"></div>
     </div>
   );
 };
